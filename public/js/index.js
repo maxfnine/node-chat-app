d9ff1d99353799f880f2266941a081ae80466fda
maxfnine <35781896+maxfnine@users.noreply.github.com>
2018-07-24 16:48:26 +0300

    Added form for messages and to show incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,25 @@ console.log('New Email arrived :',JSON.stringify(data,undefined,2));
 });
 
 socket.on('newMessage',function(data){
-    console.log('newMessage',JSON.stringify(data,undefined,2));   
+    console.log('newMessage',JSON.stringify(data,undefined,2)); 
+    var li = jQuery(`<li></li>`);
+    li.text(`${data.from} : ${data.text}`);
+    jQuery('#messages').append(li);
 })
+
+// socket.emit('createMessage',{from:'TEST',text:'TEXT'},function(){
+//     console.log("got it!");
+    
+// });
+
+jQuery('#message-form').on('submit',function(event){
+event.preventDefault();
+socket.emit('createMessage',{
+    from:'User',
+    text:jQuery('[name=message]').val()
+},function(){
+    console.log('callback called!');
+    
+});
+});
 
--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
+console.log('client connected to server');
 socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app'));
 socket.broadcast.emit('newMessage',generateMessage('Admin','New user connected'));
 
@@ -21,9 +22,11 @@ console.log('New Email created',JSON.stringify(data));
 
 });
 
-socket.on('createMessage',(data)=>{
+socket.on('createMessage',(data,callback)=>{
     console.log('createMessage',JSON.stringify(data,undefined,2));   
     io.emit('newMessage',generateMessage(data.from,data.text));
+    callback();
+   
 });
 
 socket.on('disconnect', function () {
